814e13e5a35b5c4fe0081a44371287c9bf796d8b
preitinger <peter.reitinger@gmail.com>
2022-11-16 05:23:58 +0100

    2.4 complete

--- courseinfo/src/components/Total.js
+++ courseinfo/src/components/Total.js
@@ -1,10 +1,10 @@
 const Total = ({parts}) => {
   return (
-    <p>
+    <p><strong>
     Number of exercises {
       parts.reduce((sum, part) => sum + part.exercises, 0)
     }
-    </p>
+    </strong></p>
   );
 }
 
